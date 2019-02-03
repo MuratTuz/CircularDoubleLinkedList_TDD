8e985b599846d6b69089692c472d139e5a79241c
MuratTuz <murattuzlali@gmail.com>
2019-02-03 21:44:56 +0100

    First commit

---
+++ js/Node.js
@@ -0,0 +1,31 @@
+
+/* CircularDoublyLinkedList bekledigimiz davranislar/metodlar:
+- addFirst(value), //eklenen node geri döndürülsün
+- addLast(value), //eklenen node geri döndürülsün
+- addAt(index, value), //eklenen node geri döndürülsün
+- remove(value), // silinen node geri döndürülsün
+- removeLast(), // silinen node geri döndürülsün
+- removeFirst(),// silinen node geri döndürülsün
+- removeAt(index), // silinen node geri döndürülsün
+- first(),
+- last(),
+- find(value),
+- get(index), 
+- isEmpty(), 
+- isFull()
+- size(), 
+- capacity(value),
+- capacity,
+- iterator implementasyonu
+
+Icerdeki kullancaginiz Node tipi icin bekeldiklerimiz:
+- getElement/setElement(value)
+- getNext()/setNext(pNode)
+- getPrevious()/setPrevious(pNode) */
+
+
+class Node{
+    constructor(value){
+        this.value = value
+    }
+}
